893ecf9b65c6401be20d0b91e8da90d7c449b263
= <=>
2026-01-30 21:15:14 -0300

    chore: estructura inicial del proyecto - configuración base y variables de entorno - scaffold de la app dncp_integration

---
+++ apps/dncp_integration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DncpIntegrationConfig(AppConfig):
+    name = 'apps.dncp_integration'
+    verbose_name = 'Integración DNCP'
---
+++ config/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = env.bool("DEBUG")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
